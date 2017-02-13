3b63de957621aeac2f0b9b51fa111e31eae76a0f
Victor Pontis <victor@benchling.com>
2017-02-12 19:32:44 -0800

    Add React/ as preface for dependencies

--- ios/RCTBattery/BatteryManager.m
+++ ios/RCTBattery/BatteryManager.m
@@ -5,11 +5,11 @@
 //  Created by Olajide Ogundipe Jr on 9/15/15.
 //
 
-#import "RCTBridge.h"
-#import "RCTConvert.h"
-#import "RCTEventDispatcher.h"
+#import "React/RCTBridge.h"
+#import "React/RCTConvert.h"
+#import "React/RCTEventDispatcher.h"
 
-#import "BatteryManager.h"
+#import "React/BatteryManager.h"
 
 @implementation BatteryManager
 
@@ -22,7 +22,7 @@ RCT_EXPORT_MODULE();
 {
     if ((self = [super init])) {
         [[UIDevice currentDevice] setBatteryMonitoringEnabled:YES];
-        
+
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(batteryLevelChanged:)
                                                      name:UIDeviceBatteryLevelDidChangeNotification
@@ -42,45 +42,45 @@ RCT_EXPORT_METHOD(updateBatteryLevel:(RCTResponseSenderBlock)callback)
 
 -(NSDictionary*)getBatteryStatus
 {
-    
+
     float batteryLevel = [UIDevice currentDevice].batteryLevel;
     UIDeviceBatteryState batteryState = [UIDevice currentDevice].batteryState;
-    
+
     isPlugged = FALSE;
-    
+
     NSObject* currentLevel = nil;
-    
+
     if (batteryState == UIDeviceBatteryStateCharging) {
         currentLevel = [NSNumber numberWithFloat:(batteryLevel * 100)];
         isPlugged = TRUE;
     }
-    
+
     if(batteryState == UIDeviceBatteryStateFull){
         currentLevel = [NSNumber numberWithFloat:(batteryLevel * 100)];
     }
-    
+
     if(batteryState == UIDeviceBatteryStateUnplugged){
         currentLevel = [NSNumber numberWithFloat:(batteryLevel * 100)];
     }
-    
+
     if(batteryState == UIDeviceBatteryStateUnknown || batteryState == -1.0){
         currentLevel = [NSNull null];
     } else {
         currentLevel = [NSNumber numberWithFloat:(batteryLevel * 100)];
     }
-    
+
     NSMutableDictionary* batteryData = [NSMutableDictionary dictionaryWithCapacity:2];
     [batteryData setObject:[NSNumber numberWithBool:isPlugged] forKey:@"isPlugged"];
     [batteryData setObject:currentLevel forKey:@"level"];
     return batteryData;
-    
+
 }
 
 -(void)batteryLevelChanged:(NSNotification*)notification {
-    
+
     NSDictionary* batteryData = [self getBatteryStatus];
     [self.bridge.eventDispatcher sendDeviceEventWithName:@"BatteryStatus" body:batteryData];
-    
+
 }
 
 - (void)dealloc
